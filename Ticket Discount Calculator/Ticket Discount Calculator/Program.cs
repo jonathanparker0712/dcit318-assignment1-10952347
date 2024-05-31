9b553a400a0c04433390316e0052e6c0fd1ee09e
jonathanparker0712 <102540695+jonathanparker0712@users.noreply.github.com>
2024-05-31 20:22:42 +0000

    Add files via upload

---
+++ Ticket Discount Calculator/Ticket Discount Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Price of the Ticket is Gh10");
+        Console.WriteLine("Discount allowed is Gh7");
+        Console.WriteLine("Enter age to determine whether you qualify for discount ");
+        int age = Convert.ToInt32(Console.ReadLine());
+        int price = 10;
+        int discout = 7;
+
+        if (age >= 65 || age <= 12)
+        {
+            Console.Write("Your discounted price is Gh" + (price - discout));
+        }
+        else
+        {
+            Console.WriteLine("You do not qualify for a discount");
+        }
+
+
+
+
+
+
+
+
+
+
+        
+
+
+    }
+
+}

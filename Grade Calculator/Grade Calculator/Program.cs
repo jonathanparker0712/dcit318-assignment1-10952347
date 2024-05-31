6ba6df43abf531e776b8c1f9f0fad9c1e04b140f
jonathanparker0712 <102540695+jonathanparker0712@users.noreply.github.com>
2024-05-31 19:53:40 +0000

    Add files via upload

---
+++ Grade Calculator/Grade Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter marks to determine grade ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+
+        if (marks >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (marks >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (marks >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (marks >= 60)
+        {
+            Console.WriteLine("D");
+
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+
+
+
+
+
+        
+
+
+    }
+
+}
